1d6763082e25434d21df220e08bbd6e3e392ab30
Christian Marrocco <chm@triamec.com>
2024-05-17 08:14:56 +0200

    Author project according to the Tama User Guide.

---
+++ source/HelloTamaCode.cs
@@ -0,0 +1,16 @@
+﻿using Triamec.Tama.Rlid19;       // used for register access
+using Triamec.Tama.Vmid5;        // used for interpretation of Tama attributes
+
+[Tama]                           // attribute to indicate the starting point for the Tama compiler
+static class HelloTama           // name of the tama program
+{
+    /*****************************************************************
+    * isochronous main task
+    *****************************************************************/
+    // attribute to indicate the entry point
+    [TamaTask(Task.IsochronousMain)]
+    // main function, (name is not relevant)
+    static void MainIso() {
+        Register.Application.Variables.Booleans[1] = Register.Application.Variables.Booleans[0];
+    }
+}
